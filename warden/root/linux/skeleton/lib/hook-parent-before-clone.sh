f2c8593cbdd5be249f4a12634d3c2478c364c264
yzzhu <zhuyouzhi03@163.com>
2015-06-27 16:51:09 +0800

    Update hook-parent-before-clone.sh

--- warden/root/linux/skeleton/lib/hook-parent-before-clone.sh
+++ warden/root/linux/skeleton/lib/hook-parent-before-clone.sh
@@ -6,10 +6,10 @@ set -o errexit
 shopt -s nullglob
 
 cd $(dirname $0)/../
-
+# 文件系统相关函数生效
 source ./lib/common.sh
-
+# 设置文件系统格式
 setup_fs
-
+# 拷贝wshd可执文件并修改权限 wshd属root用户
 cp bin/wshd mnt/sbin/wshd
 chmod 700 mnt/sbin/wshd
